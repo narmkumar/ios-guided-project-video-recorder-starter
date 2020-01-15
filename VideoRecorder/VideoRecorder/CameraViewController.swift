9310e2c823d35888b72a0deb663236b1318969d6
Nar Kumar <njn.kumar@gmail.com>
2020-01-15 11:03:27 -0800

    Movie Recording Functionality Complete

--- VideoRecorder/VideoRecorder/CameraViewController.swift
+++ VideoRecorder/VideoRecorder/CameraViewController.swift
@@ -7,35 +7,159 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
-
+    
+    lazy private var captureSession = AVCaptureSession()
+    lazy private var fileOutput = AVCaptureMovieFileOutput()
+    var player: AVPlayer?
+    
     @IBOutlet var recordButton: UIButton!
     @IBOutlet var cameraView: CameraPreviewView!
-
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-
-		// Resize camera preview to fill the entire screen
-		cameraView.videoPlayerView.videoGravity = .resizeAspectFill
-	}
-
-
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Resize camera preview to fill the entire screen
+        cameraView.videoPlayerView.videoGravity = .resizeAspectFill
+        setUpCamera()
+        // Add tap gesture to replay video (repeat loop)
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture(tapGesture:)))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handleTapGesture(tapGesture: UITapGestureRecognizer) {
+        print("play movie")
+        if tapGesture.state == .ended {
+            playRecording()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        captureSession.stopRunning()
+    }
+    
+    
     @IBAction func recordButtonPressed(_ sender: Any) {
-
-	}
-	
-	/// Creates a new file URL in the documents directory
-	private func newRecordingURL() -> URL {
-		let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-
-		let formatter = ISO8601DateFormatter()
-		formatter.formatOptions = [.withInternetDateTime]
-
-		let name = formatter.string(from: Date())
-		let fileURL = documentsDirectory.appendingPathComponent(name).appendingPathExtension("mov")
-		return fileURL
-	}
+        toggleRecording()
+        
+    }
+    
+    // MARK: - Methods
+    
+    func playRecording() {
+        if let player = player {
+            player.seek(to: CMTime.zero)
+            player.play()
+        }
+    }
+    
+    func updateViews() {
+        recordButton.isSelected = fileOutput.isRecording
+    }
+    
+    
+    func toggleRecording() {
+        if fileOutput.isRecording {
+            // stop
+            fileOutput.stopRecording()
+        } else {
+            fileOutput.startRecording(to: newRecordingURL(), recordingDelegate: self)
+        }
+    }
+    
+    func setUpCamera() {
+        let camera = bestCamera()
+        
+        captureSession.beginConfiguration()
+        
+        // Make changes to the devices connected
+        guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else {
+            fatalError("Cannot create camera input")
+        }
+        captureSession.addInput(cameraInput)
+        
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            captureSession.canSetSessionPreset(.hd1920x1080)
+        }
+        
+        // TODO: Audio input
+        
+        // Video output (movie)
+        guard captureSession.canAddOutput(fileOutput) else {
+            fatalError("Can't setup the file output for the movie")
+        }
+        captureSession.addOutput(fileOutput)
+        
+        captureSession.commitConfiguration()
+        cameraView.session = captureSession
+    }
+    
+    
+    /// WideAngle Lens is on every iPhone that's been shipped through 2019
+    private func bestCamera() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+            return device
+        }
+        // Fallback camera
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return device
+        }
+        fatalError("No cameras on the device. Or you are running on the Simulator (not supported)")
+    }
+    
+    
+    /// Creates a new file URL in the documents directory
+    private func newRecordingURL() -> URL {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime]
+        
+        let name = formatter.string(from: Date())
+        let fileURL = documentsDirectory.appendingPathComponent(name).appendingPathExtension("mov")
+        return fileURL
+    }
+    
+    func playMovie(url: URL) {
+        player = AVPlayer(url: url)
+        let playerLayer = AVPlayerLayer(player: player)
+        
+        var topRect = view.bounds
+        
+        topRect.size.height = topRect.height / 4
+        topRect.size.width = topRect.width / 4
+        topRect.origin.y = view.layoutMargins.top
+        
+        playerLayer.frame = topRect
+        
+        view.layer.addSublayer(playerLayer)
+        player?.play()
+    }
 }
 
+extension CameraViewController: AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        // update UI
+        updateViews()
+    }
+    
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        if let error = error {
+            print("Error saving video: \(error)")
+        }
+        print("Video: \(outputFileURL.path)")
+        updateViews()
+        
+        playMovie(url: outputFileURL)
+        
+    }
+}
--- VideoRecorder/VideoRecorder/ViewController.swift
+++ VideoRecorder/VideoRecorder/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 	
@@ -18,11 +19,43 @@ class ViewController: UIViewController {
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
 		
-		// TODO: get permission
-		
-		showCamera()
+        requestPermissionAndShowCamera()
 		
 	}
+    
+    private func requestPermissionAndShowCamera() {
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        
+        switch status {
+            
+        case .notDetermined:
+            // First time user - they haven't seen teh dialog to give permission
+            requestPermission()
+        case .restricted:
+            // Parental controls disabled the camera
+            fatalError("Video is dsaled for the user (parental controls)")
+            // TODO: Add UI to inform the user (talk to parents)
+        case .denied:
+            //User did not give us access (maybe it was an accident)
+            fatalError("Tell the user they need to enabe Privacy for videos")
+        case .authorized:
+            // we asked for permission (2nd time they've used the app)
+            showCamera()
+        @unknown default:
+            fatalError("A new status was added that we need to handle")
+        }
+    }
+    
+    private func requestPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { (granted) in
+            guard granted else {
+                fatalError("Tell user they need to enable Privacy for Video")
+            }
+            DispatchQueue.main.async { [weak self] in
+                self?.showCamera()
+            }
+        }
+    }
 	
 	private func showCamera() {
 		performSegue(withIdentifier: "ShowCamera", sender: self)
